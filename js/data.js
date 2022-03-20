a8ac38320d2e2a3941fee85a0d3f3f1d7512a80a
tienzona001 <tienzona001@gmail.com>
2022-03-20 10:53:11 +0700

    update finally

--- js/data.js
+++ js/data.js
@@ -65,7 +65,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bắp cải trắng: tên khoa học là Brassica oleracea var capitata ruba là cây bắp cải có màu tím. Xuất xứ từ Địa Trung Hải, hiện nay được trồng rộng rãi khắp thế giới, thích hợp với khí hậu ôn đới và tại Việt Nam bắp cải tím được trồng nhiều ở Đà Lạt.",
             cost: 80000,
             weight: '700g',
-            img: './img/BapCai_Trang.jpg'
+            img: '../img/BapCai_Trang.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -73,7 +73,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bắp trái tim: tên khoa học là Brassica oleracea var capitata ruba là cây bắp cải có màu tím. Xuất xứ từ Địa Trung Hải, hiện nay được trồng rộng rãi khắp thế giới, thích hợp với khí hậu ôn đới và tại Việt Nam bắp cải tím được trồng nhiều ở Đà Lạt.",
             cost: 40000,
             weight: '350g',
-            img: './img/BapCai_TraiTim.jpg'
+            img: '../img/BapCai_TraiTim.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -81,7 +81,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bắp cải tím: tên khoa học là Brassica oleracea var capitata ruba là cây bắp cải có màu tím. Xuất xứ từ Địa Trung Hải, hiện nay được trồng rộng rãi khắp thế giới, thích hợp với khí hậu ôn đới và tại Việt Nam bắp cải tím được trồng nhiều ở Đà Lạt.",
             cost: 55000,
             weight: '500g',
-            img: './img/BapCai_Tim.jpg'
+            img: '../img/BapCai_Tim.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -89,7 +89,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bông cải xanh baby có vị ngọt, mềm ăn được cả bông và thân. Giàu các vitamin C, A, canxi, folate và sắt, tốt cho trẻ nhỏ, phụ nữ mang thai và cả gia đình. Thường dùng để: xào, trộn,...",
             cost: 42000,
             weight: '250g',
-            img: './img/BongCai_Baby.jpg'
+            img: '../img/BongCai_Baby.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -97,7 +97,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bông cải xanh hoặc súp lơ xanh, là một loại cây thuộc họ cải, có hoa lớn ở đầu, thường được dùng như rau. Bông cải xanh thường được chế biến bằng cách luộc hoặc hấp, nhưng cũng có thể được ăn sống như là rau sống trong những đĩa đồ nguội khai vị.",
             cost: 57000,
             weight: '300g',
-            img: './img/BongCai_Xanh.jpg'
+            img: '../img/BongCai_Xanh.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -105,7 +105,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bông cải trắng có vị ngọt, mềm ăn được cả bông và thân. Giàu các vitamin C, A, canxi, folate và sắt, tốt cho trẻ nhỏ, phụ nữ mang thai và cả gia đình. Thường dùng để: xào, trộn,...",
             cost: 45000,
             weight: '250g',
-            img: './img/BongCai_Trang.jpg'
+            img: '../img/BongCai_Trang.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -113,7 +113,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Cải bẹ xanh có thân to, nhỏ khác nhau, lá có màu xanh đậm hoặc xanh nõn lá chuối. Lá và thân cây có vị cay, đăng đắng thường dùng phổ biến nhất là nấu canh, hay để muối dưa (dưa cải). Thời gian thu hoạch cho cải bẹ xanh trong khoảng từ 40 – 45 ngày. Thành phần dinh dưỡng trong cải bẹ xanh gồm có: vitamin A, B, C, K, axit nicotic, catoten, abumin…, nên được các chuyên gia dinh dưỡng khuyên dùng vì có nhiều lợi ích đối với sức khỏe cũng như có tác dụng phòng chống bệnh tật. Theo Đông y Việt Nam, cải bẹ xanh có vị cay, tính ôn, có tác dụng giải cảm hàn, thông đàm, lợi khí... Riêng hạt cải bẹ xanh, có vị cay, tính nhiệt, không độc, trị được các chứng phong hàn, ho đờm, hen, đau họng, tê dại, mụn nhọt.. ",
             cost: 38000,
             weight: '300g',
-            img: './img/Cai_Be_Xanh.jpg'
+            img: '../img/Cai_Be_Xanh.jpg'
         },
         {
             type: 'Bắp Và Bông Cải',
@@ -121,7 +121,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Cải bẹ xanh có thân to, nhỏ khác nhau, lá có màu xanh đậm hoặc xanh nõn lá chuối. Lá và thân cây có vị cay, đăng đắng thường dùng phổ biến nhất là nấu canh, hay để muối dưa (dưa cải). Thời gian thu hoạch cho cải bẹ xanh trong khoảng từ 40 – 45 ngày. Thành phần dinh dưỡng trong cải bẹ xanh gồm có: vitamin A, B, C, K, axit nicotic, catoten, abumin…, nên được các chuyên gia dinh dưỡng khuyên dùng vì có nhiều lợi ích đối với sức khỏe cũng như có tác dụng phòng chống bệnh tật. Theo Đông y Việt Nam, cải bẹ xanh có vị cay, tính ôn, có tác dụng giải cảm hàn, thông đàm, lợi khí... Riêng hạt cải bẹ xanh, có vị cay, tính nhiệt, không độc, trị được các chứng phong hàn, ho đờm, hen, đau họng, tê dại, mụn nhọt.. ",
             cost: 47500,
             weight: '250g',
-            img: './img/Cai_Bo_Xoi_Huu_Co.jpg'
+            img: '../img/Cai_Bo_Xoi_Huu_Co.jpg'
         },
         {
             type: 'Bầu Và Bí',
@@ -129,7 +129,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bầu xanh hữu cơ có vị ngọt, tính lạnh, có tác dụng giải nhiệt, giải độc, lợi tiểu, chữa đái dắt, đái đường. Bầu hữu cơ được trồng theo phương pháp hữu cơ, an toàn và dinh dưỡng cho người sử dụng.",
             cost: 55000,
             weight: '500g',
-            img: './img/BauBi_Xanh.jpg'
+            img: '../img/BauBi_Xanh.jpg'
         },
         {
             type: 'Bầu Và Bí',
@@ -137,7 +137,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Xuất xứ: Việt Nam Thành phần: Bí đỏ nguyên trái Hướng dẫn sử dụng: Dùng để nấu canh, soup, hấp, xào,... Hướng dẫn bảo quản: Bảo quản nơi khô ráo, thoáng mát Bí Hạt Đậu Hữu Cơ Danny Green có hình dáng giống hạt đậu, ít hạt, thịt màu vàng cam, vỏ mỏng mịn đặc biệt dẻo ngọt và đặc ruột chứa nhiều vitamin và khoáng chất thiết yếu cho cơ thể con người.",
             cost: 90000,
             weight: '1kg',
-            img: './img/BauBi_BiDo.jpg'
+            img: '../img/BauBi_BiDo.jpg'
         },
         {
             type: 'Bầu Và Bí',
@@ -145,7 +145,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bí ngòi xanh là loại trái thuộc họ bầu bí, thân tròn, dài, bên ngoài bí có màu xanh sậm, có ít vân. CÔNG DỤNG Bí ngòi nói chung giúp chữa các bệnh về hô hấp như hen suyễn, giúp tránh nhồi máu cơ tim và đột quỵ, ngăn ngừa cá bệnh về hoại huyết, thâm tím bị gây ra do thiếu hụt vitamin C. Bí ngòi còn giúp tăng khả năng ngăn ngừa chứng đa xơ cứng, ung thư ruột già, ngăn xơ vữa động mạch, làm hạ huyết áp. Một số công dụng khác như : Chống lão hóa, tăng cường trí nhớ và làm thuyên giảm những chứng bệnh liên quan đến lão hóa. Bí ngòi còn có tác dụng giảm cân vì các chất dinh dưỡng trong bí ngòi cũng có tác dụng làm tăng chuyển hóa, đồng thời loại quả này 90% là nước và có hàm lượng  calorie thấp nên giảm cân hiệu quả.",
             cost: 34500,
             weight: '300g',
-            img: './img/BauBi_BiNgoiXanhHuuCo.jpg'
+            img: '../img/BauBi_BiNgoiXanhHuuCo.jpg'
         },
         {
             type: 'Củ, Quả Tươi',
@@ -153,7 +153,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Dưa chuột (hay dưa leo) là một loại rau ăn quả quen thuộc đối với người Việt Nam. Dưa chuột rất mát, vị giòn, ngon ngọt, thơm hấp dẫn. Dưa chuột có tác dụng thanh nhiệt, giải khát, mát da thịt, lợi tiểu, chữa phù thũng, sưng trướng, kiết lỵ do nhiệt, đau bụng do ruột bị kích thích, chống lại các bệnh ung thư và dưỡng da. Dưa chuột có hàm lượng canxi cao nên có tác dụng tốt đối với trẻ em chậm lớn. Ngoài ra, dưa chuột còn có tác dụng hỗ trợ tiêu hóa, giảm cân và giúp thận khỏe mạnh. Món ngon với dưa chuột: dưa chuột trộn chua cay, dưa chuột chấm muối ớt, dưa chuột trộn phổ tai, dưa chuột xào chả cá, tôm sú xào dưa chuột hạt điều, dưa chuột xào thịt băm, dưa chuột trộn sò điệp, salad dưa chuột…",
             cost: 24000,
             weight: '300g',
-            img: './img/CuQua_DuaLeo.jpg'
+            img: '../img/CuQua_DuaLeo.jpg'
         },
         {
             type: 'Củ, Quả Tươi',
@@ -161,7 +161,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bắp nếp là một loại ngũ cốc dễ ăn, dễ trồng được mệnh danh là Ngọc Mễ (Hạt ngọc quý) đối với người Việt xưa. Khi mà xã hội ngày càng phát triển, nhiều loại hạt và ngũ cốc được khoa học chứng minh về mức độ dinh dưỡng cùng khả năng phòng chống bệnh tật, bắp nếp cũng là một trong những loại trái cây như thế.",
             cost: 110000,
             weight: '1kg',
-            img: './img/CuQua_BapNep.jpg'
+            img: '../img/CuQua_BapNep.jpg'
         },
         {
             type: 'Củ, Quả Tươi',
@@ -169,7 +169,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Ăn gừng thường xuyên phòng chữa sỏi mật Sử dụng gừng thường xuyên rất có lợi cho việc phòng, chữa sỏi mật. Sỏi mật hiện nay thường được điều trị bằng phẫu thuật và chưa có một thuốc đặc trị nào có hiệu quả. Nghiên cứu mới đây của các nhà khoa học Nhật Bản cho biết: Đã phát hiện thấy thành phần các chất có trong vị cay của gừng tươi như 6-Zingiberol, Ginger oil và 4 chất khác phân tách ra được, đều có tác dụng ức chế hợp thành Prostaglana – chất gây ra sỏi mật. Khi lượng prostaglandin (PG) trong cơ thể phân tiết quá nhiều, hàm lượng chất muxin (một loại protein) trong dịch mật có thể sẽ tăng lên. ",
             cost: 21900,
             weight: '100g',
-            img: './img/CuQua_Gung.jpg'
+            img: '../img/CuQua_Gung.jpg'
         },
         {
             type: 'Củ, Quả Tươi',
@@ -177,7 +177,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Cà rốt là loại cây có củ, củ to ở phần đầu và nhọn ở phần đuôi, củ cà rốt thường có màu cam hoặc đỏ, phẩn ăn được thường gọi là củ nhưng thực chất đó là phần rễ của cà rốt. ",
             cost: 41400,
             weight: '300g',
-            img: './img/CuQua_Carot.jpg'
+            img: '../img/CuQua_Carot.jpg'
         },
         {
             type: 'Rau Xanh',
@@ -185,7 +185,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "ây diếp cá có thân màu lục hoặc tím đỏ, lá mọc so le, hình tim, có bẹ, khi vò ra có mùi tanh như mùi cá. Diếp cá có vị cay chua, mùi tanh, tính mát, có tác dụng thanh nhiệt giải độc, lợi tiểu tiêu thũng, sát trùng, còn có tác dụng ức chế thần kinh và chống viêm loét.",
             cost: 26000,
             weight: '100g',
-            img: './img/Rau_DiepCa.jpg'
+            img: '../img/Rau_DiepCa.jpg'
         },
         {
             type: 'Rau Xanh',
@@ -193,7 +193,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Bạc hà tây được dùng làm gia vị vì nó có mùi thơm đặc trưng. Ngoài ra bạc hà còn được điều chế làm tinh dầu... Cần để bạc hà tây trong bao kín và bảo quản trong tủ lạnh.",
             cost: 30000,
             weight: '100g',
-            img: './img/Rau_BacHa.jpg'
+            img: '../img/Rau_BacHa.jpg'
         },
         {
             type: 'Rau Xanh',
@@ -201,7 +201,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Húng quế là cây rau xanh, lá nhỏ, cành tím, có hoa màu trắng. Húng quế có các loại húng quế, húng quế ngọt, húng chanh, húng quế hồi.     CÔNG DỤNG Húng quế có lợi cho sức khỏe: Tốt cho hệ tiêu hoá, kháng khuẩn, ngăn ngừa lão hóa, lợi sữa, giảm cholesterol, giảm đường trong máu, ngăn ngừa ung thư, trị mụn trứng cá,…",
             cost: 15000,
             weight: '60g',
-            img: './img/Rau_HungHue.jpg'
+            img: '../img/Rau_HungHue.jpg'
         },
         {
             type: 'Rau Xanh',
@@ -209,7 +209,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Xà lách Romaine cây có màu xanh tươi, ăn rất giòn, có vị ngọt và thơm, hiện đang được trồng tại một số trang trại rau cao cấp ở Đà Lạt. Xà lách Romaine giàu Vitamin A, Vitamin K, Vitamin C, Magiê, chất xơ và ít protein. Xà lách Romaine có tác dụng hỗ trợ tiêu hóa và tốt cho gan, giảm nguy cơ mắc bệnh tim mạch, các cơn nhồi máu cơ tim, ung thư, nứt cột sống, thiếu máu, chứng mất ngủ do căng thẳng, hỗ trợ tiêu hóa và tốt cho gan, giảm nguy cơ mắc bệnh tim mạch, các cơn nhồi máu cơ tim, ung thư, nứt cột sống, thiếu máu, chứng mất ngủ do căng thẳng. Ngoài ra Vitamin C và Beta –Carotene kết hợp với nhau để phòng chống sự  oxy  hóa  cholesterol.. Để bảo quản được lâu, cần  rửa sạch, để ráo nước, sau đó cho vào túi nilong và lưu trữ trong tủ lạnh ở nhiệt độ 5-12°C. Cách sử dụng: Có thể làm món salad trộn hoặc ăn sống  với các món khác… Xà lách Romaine hữu cơ được trồng tại vườn hữu cơ tại Đà Lạt, Lâm Đồng. Xà lách Romaine có màu xanh tươi, ăn rất giòn, có vị ngọt và thơm.",
             cost: 58000,
             weight: '300g',
-            img: './img/RauXanh_XaLach.jpg'
+            img: '../img/RauXanh_XaLach.jpg'
         },
         {
             type: 'Ớt, Ớt Chuông',
@@ -217,7 +217,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Ớt hiểm tại Organicfood phân phối dựa trên hoàn toàn nguồn ớt hiểm hữu cơ, nói không hóa chất độc hại, không phân bón hóa học, thuốc trừ sâu, thuốc biến đổi gen hay kích thích tăng trưởng,…không những đảm bảo tiêu chuẩn bảo vệ sực khỏe của người sử dụng mà còn giữ nguyên vị ngon, cay xé lưỡi, làm món ăn thêm hấp dẫn   ",
             cost: 19000,
             weight: '100g',
-            img: './img/Ot_OtHiem.jpg'
+            img: '../img/Ot_OtHiem.jpg'
         },
         {
             type: 'Ớt, Ớt Chuông',
@@ -225,7 +225,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Ớt tây có 3 màu: xanh, vàng, đỏ. Nó vừa giàu chất xơ mà lại ít kalo.",
             cost: 48000,
             weight: '300g',
-            img: './img/Ot_OtChuongVang.png'
+            img: '../img/Ot_OtChuongVang.png'
         },
         {
             type: 'Ớt, Ớt Chuông',
@@ -233,7 +233,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Ớt tây có 3 màu: xanh, vàng, đỏ. Nó vừa giàu chất xơ mà lại ít kalo.",
             cost: 48000,
             weight: '300g',
-            img: './img/Ot_Otchuongxanh.jpg'
+            img: '../img/Ot_Otchuongxanh.jpg'
         },
         {
             type: 'Ớt, Ớt Chuông',
@@ -241,7 +241,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Ớt tây có 3 màu: xanh, vàng, đỏ. Nó vừa giàu chất xơ mà lại ít kalo.",
             cost: 48000,
             weight: '300g',
-            img: './img/Ot_OtChuongDo.jpg'
+            img: '../img/Ot_OtChuongDo.jpg'
         },
 
         {
@@ -250,7 +250,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Nấm kim châm là một loài nấm màu trắng được sử dụng trong ẩm thực các nước châu Á như Hàn Quốc, Nhật Bản, Triều Tiên. Loại nấm này theo truyền thống được sử dụng nấu món lẩu, nhưng cũng có thể được sử dụng cho món salad và các món ăn khác.",
             cost: 26000,
             weight: 'Bó',
-            img: './img/Nam_KimCham.jpg'
+            img: '../img/Nam_KimCham.jpg'
         },
         {
             type: 'Nấm Tươi',
@@ -258,7 +258,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Cách bảo quản: cắt bỏ phần chân nấm, nhúng qua nước sôi từ 1 - 2 phút, sau đó rửa lại bằng nước lạnh, để ráo nước rồi cho vào hộp kín rồi bỏ vào tủ lạnh, với cách này có thể bảo quản được nấm từ 3 - 4 ngày. ",
             cost: 78000,
             weight: '250g',
-            img: './img/Nam_DuoiGa.jpg'
+            img: '../img/Nam_DuoiGa.jpg'
         },
         {
             type: 'Nấm Tươi',
@@ -266,7 +266,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Cách bảo quản: cắt bỏ phần chân nấm, nhúng qua nước sôi từ 1 - 2 phút, sau đó rửa lại bằng nước lạnh, để ráo nước rồi cho vào hộp kín rồi bỏ vào tủ lạnh, với cách này có thể bảo quản được nấm từ 3 - 4 ngày. ",
             cost: 49000,
             weight: '200g',
-            img: './img/Nam_Baby.jpg'
+            img: '../img/Nam_Baby.jpg'
         },
         {
             type: 'Nấm Tươi',
@@ -274,7 +274,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Nấm đông cô hay còn gọi là nấm hương là một loại nấm ăn có nguồn gốc bản địa ở Đông Á. Nấm đông cô có dạng như cái ô, đường kính 4–10 cm, màu nâu nhạt, khi chín chuyển thành nâu sậm. Nấm đông cô có một chân đính vào giữa tai nấm. Mặt trên tai nấm màu nâu, mặt dưới có nhiều bản mỏng xếp lại.Trên mặt nấm có những vảy nhỏ màu trắng. Thịt nấm màu trắng, cuống hình trụ. ",
             cost: 100000,
             weight: '300g',
-            img: './img/Nam_DongCo.jpg'
+            img: '../img/Nam_DongCo.jpg'
         },
         {
             type: 'Khoai',
@@ -282,7 +282,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Khoai Tây có ruột màu vàng, thân gồ ghề, củ hơi tròn, vị ngọt, dẻo/ bùi. Khoai Tây không những là một loại thực phẩm bổ dưỡng mà nó còn có tác dụng chữa bệnh khiến bạn ngạc nhiên. ",
             cost: 70000,
             weight: '1kg',
-            img: './img/Khoai_Tay.jpg'
+            img: '../img/Khoai_Tay.jpg'
         },
         {
             type: 'Khoai',
@@ -290,7 +290,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Không chỉ có màu sắc và mùi vị đặc trưng, khoai tây hồng còn là một loại thực phẩm dinh dưỡng có rất nhiều tác dụng chữa bệnh. Nguyên nhân là vì trong khoai tây hồng có chứa rất nhiều chất dinh dưỡng có thể kể đến như: tinh bột, Vitamin C, Vitamin B6, BI, B2, Kali, Phốt pho, chất xơ, chất chống oxy hóa,… những chất này đã góp phần tạo nên hương vị cũng như công dụng của khoai lang hồng. ",
             cost: 40500,
             weight: '300g',
-            img: './img/khoai_khoaitayhongorganic300g.png'
+            img: '../img/khoai_khoaitayhongorganic300g.png'
         },
         {
             type: 'Khoai',
@@ -298,7 +298,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Khoai môn là loại củ quen thuộc trong ẩm thực Việt, nấu được cả món mặn và ngọt: chè khoai môn, canh khoai môn, bánh ngọt... ",
             cost: 42500,
             weight: '500g',
-            img: './img/khoai_khoaimonhuuco500g.jpg'
+            img: '../img/khoai_khoaimonhuuco500g.jpg'
         },
         {
             type: 'Khoai',
@@ -306,7 +306,7 @@ if(typeof(Storage) !== 'undefined') {
             infor: "Khoai môn là loại củ quen thuộc trong ẩm thực Việt, nấu được cả món mặn và ngọt: chè khoai môn, canh khoai môn, bánh ngọt... ",
             cost: 39000,
             weight: '500g',
-            img: './img/khoai_khoailangnhathuuco500g.jpg'
+            img: '../img/khoai_khoailangnhathuuco500g.jpg'
         },
         
 
